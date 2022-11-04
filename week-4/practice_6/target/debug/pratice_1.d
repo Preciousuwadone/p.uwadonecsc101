1ca027e924f2412998af4e67db86eb9fe7657d87
unknown <precious.uwadone@pau.edu.ng>
2022-11-04 18:06:19 +0100

    (My fourth commit)

---
+++ week-4/practice_6/target/debug/pratice_1.d
@@ -0,0 +1,1 @@
+C:\Users\Precious\ Uwadone\OneDrive\Documents\p.uwadoneCSC101\week-4\practice_6\target\debug\pratice_1.exe: C:\Users\Precious\ Uwadone\OneDrive\Documents\p.uwadoneCSC101\week-4\practice_6\src\main.rs
---
+++ week-4/practice_8/target/debug/pratice_1.d
@@ -0,0 +1,1 @@
+C:\Users\Precious\ Uwadone\OneDrive\Documents\p.uwadoneCSC101\week-4\practice_8\target\debug\pratice_1.exe: C:\Users\Precious\ Uwadone\OneDrive\Documents\p.uwadoneCSC101\week-4\practice_8\src\main.rs
