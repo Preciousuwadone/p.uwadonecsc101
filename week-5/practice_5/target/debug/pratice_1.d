1b52b5a69dcf777bdaa5e4188aa565f6b0f3db36
unknown <precious.uwadone@pau.edu.ng>
2022-11-18 11:40:55 +0100

    (My Fifth Commit)

---
+++ week-5/practice_5/target/debug/pratice_1.d
@@ -0,0 +1,1 @@
+C:\Users\Precious\ Uwadone\OneDrive\Documents\p.uwadoneCSC101\week-5\practice_5\target\debug\pratice_1.exe: C:\Users\Precious\ Uwadone\OneDrive\Documents\p.uwadoneCSC101\week-5\practice_5\src\main.rs
---
+++ week-5/project_1/target/debug/pratice_1.d
@@ -0,0 +1,1 @@
+C:\Users\Precious\ Uwadone\OneDrive\Documents\p.uwadoneCSC101\week-5\project_1\target\debug\pratice_1.exe: C:\Users\Precious\ Uwadone\OneDrive\Documents\p.uwadoneCSC101\week-5\project_1\src\main.rs
